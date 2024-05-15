5b0c6342f08d308b1d017b5d76876cba81f92495
garynotethan <huangethan93@gmail.com>
2024-05-14 23:58:37 -0700

    feat: added cmds_dir to settings

--- settings.py
+++ settings.py
@@ -1,6 +1,11 @@
+import pathlib
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
+
+BASE_DIR = pathlib.Path(__file__).parent
+
+CMDS_DIR = BASE_DIR / "commands"
