cc1c07d9497c41c02fc7c96c980e3547bfcce5a8
garynotethan <huangethan93@gmail.com>
2025-06-01 23:23:00 -0700

    feat: made commands user installable
    added the decorator to every command

--- cogs/servers.py
+++ cogs/servers.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-
+from discord import app_commands
 
 class Servers(commands.Cog):
 
@@ -9,46 +9,57 @@ class Servers(commands.Cog):
 
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def planet(self, ctx):
         await ctx.send('https://discord.gg/5thTVupNHh')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def workshop(self, ctx):
         await ctx.send('https://discord.com/invite/7F4BjWD')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def ipmc(self, ctx):
         await ctx.send('https://discord.gg/wby7U2g')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def mpsd(self, ctx):
         await ctx.send('https://discord.gg/347xyYa')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def reddit(self, ctx):
         await ctx.send('https://discord.com/invite/010bWcQCla9sRdYea')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def psuk(self, ctx):
         await ctx.send('https://discord.gg/AE2WQrz5JM')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def kronen(self, ctx):
         await ctx.send('https://discord.gg/AZDeABYj74')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def spindys(self, ctx):
         await ctx.send('https://discord.gg/VzyYnfECnS')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def fenspinner(self, ctx):
         await ctx.send('https://discord.gg/Fz2zHWjn8G')
 
     @commands.hybrid_command(aliases=['india'])
+    @app_commands.user_install()
     async def ibps(self, ctx):
         await ctx.send('https://discord.gg/tmFJMsbp7u')
 
     @commands.hybrid_command(aliases=['krug'])
+    @app_commands.user_install()
     async def krugdom(self, ctx):
         await ctx.send('https://discord.gg/eMQSvwDd5Q')
 
--- cogs/videos.py
+++ cogs/videos.py
@@ -1,64 +1,78 @@
 import discord
 from discord.ext import commands
-
+from discord import app_commands
 class Videos(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def sls(self, ctx):
         await ctx.send('https://www.youtube.com/playlist?list=PLqvo4PPYx7nU8j3Cjz2u6dZ7s0FERNJ7y')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def links1(self, ctx):
         await ctx.send('https://www.youtube.com/watch?v=4RZDt9B4sfQ')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def links2(self, ctx):
         await ctx.send('https://www.youtube.com/watch?v=24CxVVMact4')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def cvs(self, ctx):
         await ctx.send('https://www.youtube.com/playlist?list=PLE4F7122D79474299 I recommend watching a bunch of cvs to get some inspiration! It\'s also a great way to develop spinning preferences and see potential areas you want to explore.')
 
     @commands.hybrid_command(aliases=['sonic'])
+    @app_commands.user_install()
     async def sonictut(self, ctx):
         await ctx.send('https://youtu.be/IyfvQbepErw')
 
     @commands.hybrid_command(aliases=['bust', 'busttutorial'])
+    @app_commands.user_install()
     async def busttut(self, ctx):
         await ctx.send('These are gary approved bust tutorials. \n https://www.youtube.com/watch?v=TUYpVo14lWo \n https://www.youtube.com/watch?v=0yL7hn011V8&t \n https://www.youtube.com/watch?v=UvyNfif6Kdo')
 
     @commands.hybrid_command(aliases=['g2', 'g2removal', 'g2grips'])
+    @app_commands.user_install()
     async def g2grip(self, ctx):
         await ctx.send('https://www.youtube.com/watch?v=HNZhY6q8Aw0')
 
     @commands.hybrid_command(aliases=['walmartmod', 'walmart', 'walmartmodv1', 'garywalmartmod1', 'walmartmod1', ])
+    @app_commands.user_install()
     async def garywalmartmod(self, ctx):
         await ctx.send('https://www.youtube.com/watch?v=xJl72ZisCVw')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def links3(self, ctx):
         await ctx.send('https://www.youtube.com/watch?v=sJjx797j1x8')
     
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def links(self, ctx):
         await ctx.send('https://www.youtube.com/watch?v=4RZDt9B4sfQ\nhttps://www.youtube.com/watch?v=24CxVVMact4\nhttps://www.youtube.com/watch?v=sJjx797j1x8')
     
     @commands.hybrid_command(aliases=['staplesmod', 'staples', 'staplesmodv1', 'garystaplesmod1', 'staplesmod1',])
+    @app_commands.user_install()
     async def garystaplesmod(self, ctx):
         await ctx.send('https://youtu.be/xgSxwK2zYpk')
 
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def essentials(self, ctx):
         await ctx.send('This is playlist contains Gary\'s favourite penspinning videos and stuff he would recommend noobs watch aswell. https://youtube.com/playlist?list=PLkPBnIe5Br650sNllwRwlURZeBZDAjSrb')
         
     @commands.hybrid_command()
+    @app_commands.user_install()
     async def chopstick(self, ctx):
         await ctx.send('https://youtu.be/9bAevHmu9oE')
     
     @commands.hybrid_command(aliases=['st'])
+    @app_commands.user_install()
     async def supertip(self, ctx):
         await ctx.send('https://youtu.be/ep6MSDCFHm0')
     
